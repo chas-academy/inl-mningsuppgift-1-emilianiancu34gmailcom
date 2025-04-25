859ae7f9e1293b5cbac1b62412488898a77f2873
emilianiancu34@gmail.com <emilianiancu34@gmail.com>
2025-04-26 01:04:18 +0200

    making the diceroll and calculate the sum

--- main.c
+++ main.c
@@ -5,7 +5,13 @@ int main()
 {
     // The Line below must *NOT* be changed or test will fail!
     srand(1);
-
+    int diceroll[100],one=0,two=0,three=0,four=0,five=0,six=0,i,s=0; 
+    srand(1);
+    for(i=0;i<100;i++)
+    {
+        diceroll[i]=(rand()%6)+1;
+        s+=diceroll[i];
+    }
    
     // Your code goes here
 
